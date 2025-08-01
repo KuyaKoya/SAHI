ca27faf263ab2a30416f00387404ae8b372c5f15
Seth Cornelio <seth.cornelio@mllrdev.com>
2025-08-01 09:25:54 +0800

    feat: add training on best model

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 
 
 def train():
-    model = YOLO("yolov8m.yaml")  # or yolov8m/l for better performance
+    model = YOLO("yolov8n.yaml")  # or yolov8m/l for better performance
     model.train(
         data="data/floorplans-roboflow-yolov11/data.yaml",
         epochs=100,
@@ -15,5 +15,22 @@ def train():
     )
 
 
+def improvement_train():
+    model = YOLO("model/model_v52.pt")  # or the path of your best model
+    model.train(
+        data="data/floorplans-roboflow-yolov11/data.yaml",
+        epochs=100,
+        imgsz=1280,
+        batch=8,
+        workers=8,
+        optimizer="Adam",
+        patience=60,
+        project="exports",
+        name="floorplans_yolov11",
+        exist_ok=True,
+    )
+
+
 if __name__ == "__main__":
-    train()
+    # train()
+    improvement_train()
